e51b77593309cb585883b1b7388e285b5f3be130
MyMatrixs <wangwanli438@gmail.com>
2023-11-27 21:19:16 +0800

    把三国杀表情包放上去

--- pages/Gallery.js
+++ pages/Gallery.js
@@ -1,18 +1,22 @@
-function ProFile(){
+function PicSanguosha({number}){
+    const imgurl = `/image/sanguosha/${number.toString().padStart(2,'0')}.jpg`;
     return(
       <img
-        src='https://i.imgur.com/QIrZWGIs.jpg'
-        alt='Abc'
+        src={imgurl}
+        alt={`Sanguosha ${number}`}
       ></img>
     )
   }
 
   export default function Gallery(){
+    const images = [];
+    for(let i=1;i<=49;i++){
+      images.push(<PicSanguosha key = {i} number={i}/>)
+    }
     return(
         <section>
-            <ProFile></ProFile>
-            <ProFile></ProFile>
-            <ProFile></ProFile>
+            <h1>三国杀表情包自取</h1>
+            {images}
         </section>
     )
   }
--- pages/index.js
+++ pages/index.js
@@ -9,33 +9,18 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Welcome Matrix's website</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
           <Gallery></Gallery>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        <p>by wangwanli</p>
       </footer>
 
       <style jsx>{`
