f0088719d18f16727650c172e6285908920aec42
MyMatrixs <wangwanli438@gmail.com>
2023-11-28 17:41:58 +0800

    添加声音

--- pages/Gallery.js
+++ pages/Gallery.js
@@ -5,9 +5,25 @@ import styles from '../styles/Mystyle.module.css'; // 引入CSS样式文件
 function PicSanguosha({number}){
     const imgurl = `${apiUrl}/image/sanguosha/${number.toString().padStart(2,'0')}.jpg`;
     const [isJumping, setIsJumping] = useState(false);
-
+    const playBeepSound = () => {
+      const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+      const oscillator = audioContext.createOscillator();
+      const gainNode = audioContext.createGain();
+      
+      
+      oscillator.connect(gainNode);
+      gainNode.connect(audioContext.destination);
+      oscillator.type = 'sine';
+      oscillator.frequency.setValueAtTime(3000, audioContext.currentTime);
+      oscillator.frequency.exponentialRampToValueAtTime(50, audioContext.currentTime + 0.3);
+      gainNode.gain.setValueAtTime(1, audioContext.currentTime);
+      gainNode.gain.exponentialRampToValueAtTime(0.1, audioContext.currentTime + 0.2);
+      oscillator.start(audioContext.currentTime);
+      oscillator.stop(audioContext.currentTime + 0.3);
+    };
     const handleClick = () => {
       setIsJumping(true);
+      playBeepSound(); // 播放声音
       // 设置定时器以在动画完成后重置状态
       setTimeout(() => setIsJumping(false), 300); // 假设动画时长为500毫秒
     };
