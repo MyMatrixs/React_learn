37bdfb247cef2b442af2eee4fba446065ca79d53
MyMatrixs <wangwanli438@gmail.com>
2026-02-05 00:44:03 +0800

    修复next不兼容问题

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    transpilePackages: [
+        'antd',
+        '@ant-design/icons',
+        '@ant-design/icons-svg',
+        '@rc-component/util',
+    ],
 }
 
 module.exports = nextConfig
